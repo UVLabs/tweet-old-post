08a585a7b9de91db44b40d7512bc731f11cd7280
UVLabs <uriahs.victor@gmail.com>
2021-05-28 15:05:47 -0400

    phpcs fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,46 +3,47 @@
  *
  * @package tweet-old-post
  */
- module.exports = function (grunt) {
-    grunt.initConfig({
-        wp_readme_to_markdown: {
-            files: {
-                'readme.md': 'readme.txt'
-            },
-        },
-        version: {
-
-            project: {
-                src: [
-                    'package.json'
-                ]
-            },
-            style: {
-                options: {
-                    prefix: 'Version\\:\.*\\s'
-                },
-                src: [
-                    'tweet-old-post.php',
-                ]
-            },
-            functions: {
-                options: {
-                    prefix: 'version\\s+=\\s+[\'"]'
-                },
-                src: [
-                    'includes/class-rop.php',
-                ]
-            },
-            constants: {
-                options: {
-                    prefix: 'ROP_LITE_VERSION\'\,\\s+\''
-                },
-                src: [
-                    'tweet-old-post.php',
-                ]
-            }
-        }
-    });
-    grunt.loadNpmTasks('grunt-version');
-    grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
+module.exports = function (grunt) {
+	grunt.initConfig(
+		{
+			wp_readme_to_markdown: {
+				files: {
+					'readme.md': 'readme.txt'
+				},
+			},
+			version: {
+				project: {
+					src: [
+					'package.json'
+					]
+				},
+				style: {
+					options: {
+						prefix: 'Version\\:\.*\\s'
+					},
+					src: [
+					'tweet-old-post.php',
+					]
+				},
+				functions: {
+					options: {
+						prefix: 'version\\s+=\\s+[\'"]'
+					},
+					src: [
+					'includes/class-rop.php',
+					]
+				},
+				constants: {
+					options: {
+						prefix: 'ROP_LITE_VERSION\'\,\\s+\''
+					},
+					src: [
+					'tweet-old-post.php',
+					]
+				}
+			}
+		}
+	);
+	grunt.loadNpmTasks( 'grunt-version' );
+	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
 };
